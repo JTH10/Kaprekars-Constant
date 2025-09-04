086f85157bd185305ec202a7774f41e694bfdd4f
JTH10 <jthenrie@yahoo.com>
2025-09-04 15:42:05 -0600

    refactor: modularize Kaprekar routine, simplify input validation, and clean up output formatting

--- kc.py
+++ kc.py
@@ -1,55 +1,33 @@
-import string
-import math
-import re
+# Kaprekar's Constant finder (6174 routine)
 
+def is_valid(num): 
+    # Validate input: must be a 4-digit number with at least two unique digits
+    return len(num) == 4 and num.isdigit() and len(set(num)) > 1
+ 
+def kaprekars_f(n):
+    # Perform one step of Kaprekar's routine by subtracting descending number from ascending number.
+    desc = int(''.join(sorted(n, reverse=True))) 
+    asc = int(''.join(sorted(n))) 
+    result = desc - asc 
+    return str(result).zfill(4)  # Ensure result is 4 digits with leading zeros
 
 def main():
+    while True: 
+        num = input("Enter a 4-digit number (at least two different digits): ") 
+        if is_valid(num): 
+            break 
+        print("Invalid input. Please try again.")
 
-    while True:
-        num = input("Give me a 4 digit number: ")
-        #if len(num) == 4 and num.isdigit() and num != "0000" and num != "1000" and num != "0100" and num != "0010" and num != "0001" and num != "9998" and num != "8889":
-        #if is_valid_number(num):
-        temp_num = sorted(num)
-        temp_num_rev = sorted(num,reverse=True)
-        #temp_num = ''.join(temp_num)
-        pattern = re.compile(r"^(?!(000[01]|111[0-2]|222[1-3]|333[2-4]|444[3-5]|555[4-6]|666[5-7]|777[6-8]|888[7-9]|999[89]))\d{4}$")
-        if pattern.match(''.join(temp_num)) and pattern.match(''.join(temp_num_rev)):
-            print(num)
-            break
-        else:
-            print("Invalid input. Try again.")
-
- 
-#Stumped...Figure out how to convert a int into a string.
-    while True:
-        temp_y = sorted(num)
-        temp_x = sorted(num,reverse=True)
-        new_num = kaprekars_f(num)
-        print( ''.join(temp_x), "-", ''.join(temp_y), "=", new_num)
-        num = new_num
-
-        if new_num == "6174" or new_num == "0":
-            break
-
-
-def kaprekars_f(n):
-   x2 = sorted(n, reverse=True)
-   y2 = sorted(n)
-   y1 = "".join(y2)
-   x1 = "".join(x2) 
-   y = int(y1)
-   x = int(x1)
-   if y > x:
-        z = y - x
-        return str(z)
-   else:
-        z = x - y
-        return str(z)
-
-
-#def is_valid_number(number):
-#    pattern = re.compile(r'^(?!(\d)\1{3})(?!(\d)\d\1(?:\1[+-]1|[+-]\d\1))\d{4}$')
-#    return True if pattern.match(number) else False
+    steps = 0 
+    while num != "6174": 
+        x = ''.join(sorted(num, reverse=True)) 
+        y = ''.join(sorted(num)) 
+        new_num = kaprekars_f(num) 
+        print(f"{x} - {y} = {new_num}") 
+        num = new_num 
+        steps += 1 
+        
+    print(f"You've reached Kaprekar's constant in {steps} steps!")
     
 if __name__ == '__main__':
     main()
